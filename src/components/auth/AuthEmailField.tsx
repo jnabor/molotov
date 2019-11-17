346cd37446045166db0111c1d9d896c01bc3034d
sonab <sonabstudios@gmail.com>
2019-11-18 07:57:36 +0800

    added email validation

--- src/components/auth/AuthEmailField.tsx
+++ src/components/auth/AuthEmailField.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import _ from 'lodash'
 
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -9,11 +10,30 @@ import {
   Theme
 } from '@material-ui/core/styles'
 
+interface EmailState {
+  email: string
+  valid: boolean
+}
+
 export interface AuthEmailFieldProps {}
 
-const AuthEmailField: React.SFC<AuthEmailFieldProps> = ({ children }) => {
+const AuthEmailField: React.SFC<AuthEmailFieldProps> = () => {
+  const [state, setState] = useState<EmailState>({
+    email: '',
+    valid: true
+  })
+
+  const validate = (email: string): any => {
+    console.log('validating')
+    setState({
+      ...state,
+      valid: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email) ? true : false
+    })
+  }
+
   return (
     <TextField
+      error={!state.valid}
       variant='outlined'
       margin='normal'
       required
@@ -22,6 +42,7 @@ const AuthEmailField: React.SFC<AuthEmailFieldProps> = ({ children }) => {
       label='Email Address'
       name='email'
       autoComplete='email'
+      onChange={e => validate(e.currentTarget.value)}
       autoFocus
     />
   )
